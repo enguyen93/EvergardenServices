b480aa4e38edca4fd8a61019b74cdfe50790d259
Eric Nguyen <enguyenusaf93@gmail.com>
2019-02-07 20:22:01 -0600

    added redux devtools

--- client/src/store.js
+++ client/src/store.js
@@ -8,9 +8,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.REDUX_DEVTOOLS_EXTENSION_COMPOSE ? window.REDUX_DEVTOOLS_EXTENSION_COMPOSE({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      }) : compose
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
