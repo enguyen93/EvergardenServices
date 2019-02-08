fd813289da542c5a7a5ad55330f88eceaf49fe43
Eric Nguyen <enguyenusaf93@gmail.com>
2019-02-07 18:49:10 -0600

    help

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: "mongodb://test123:testtest1@ds223685.mlab.com:23685/heroku_5pl3nw5k",
-    secretOrKey: process.env.SECRET || "secret" 
+    secretOrKey: process.env.SECRET || "secret",
   };
